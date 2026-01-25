9c601ae01bd9f13247bbe7ec376120ca84537cfa
Vu Huu Nguyen Khoi <162899678+NgyenKhoi@users.noreply.github.com>
2026-01-25 05:58:44 +0000

    add backend codebase

---
+++ backend/src/main/java/com/example/backend/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.example.demo.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public enum ErrorCode {
+    UNEXPECTED_EXCEPTION(9999, "undefined exception", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    private int code;
+    private String message;
+    private HttpStatusCode statusCode;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
